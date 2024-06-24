dba17c9bc085f893a98a6556ca982420ac2154a2
Wesley Rosa <wesleymrosa@gmail.com>
2024-06-24 17:59:42 -0300

    EndPoint de upload funcionando.

---
+++ src/main/java/br/com/upload_arquivos_api/controller/UploadArquivoController.java
@@ -0,0 +1,43 @@
+package br.com.upload_arquivos_api.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+@RestController
+@RequestMapping (value = "api/upload", produces = {"application/json"})
+@Slf4j
+@CrossOrigin ("*")
+public class UploadArquivoController {
+@PostMapping ("/arquivo")
+    public ResponseEntity<String> salvarArquivo (@RequestParam ("file")MultipartFile file){
+
+        log.info("Recebendo o arquivo: ", file.getOriginalFilename());
+        var path = "C:\\Users\\wesley.rosa\\workspaceIntellij\\upload-arquivos-api\\src\\uploads\\";
+//        var caminho = path + file.getOriginalFilename() +"." + extrairExtensao(file.getOriginalFilename());
+    var caminho = path + file.getOriginalFilename(); //+ "." + extrairExtensao(file.getOriginalFilename());
+
+        log.info("Novo nome do arquivo: " + caminho);
+
+        try {
+            Files.copy(file.getInputStream(), Path.of(caminho), StandardCopyOption.REPLACE_EXISTING);
+            return new ResponseEntity<>("{ \"mensagem\": \"Arquivo carregado com sucesso !\"}",HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Erro ao processar arquivo", e);
+            return new ResponseEntity<>("{ \"mensagem\": \"Erro ao carregar o arquivo !\"}",HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private String extrairExtensao(String nomeArquivo) {
+        int i = nomeArquivo.lastIndexOf(".");
+        return nomeArquivo.substring(i +1);
+    }
+}
